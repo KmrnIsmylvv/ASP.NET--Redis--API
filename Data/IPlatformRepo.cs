c37e3c210c1d374b6775490ba33bed44df2a001b
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-12-29 16:48:40 +0400

    feat: Implement interface

--- Data/IPlatformRepo.cs
+++ Data/IPlatformRepo.cs
@@ -5,7 +5,7 @@ namespace RedisAPI.Data
     public interface IPlatformRepo
     {
         void CreatePlatform(Platform platform);
-        Platform GetPlatformById(string id);
+        Platform? GetPlatformById(string id);
         IEnumerable<Platform> GetAllPlatforms();
     }
 }
--- Data/RedisPlatformRepo.cs
+++ Data/RedisPlatformRepo.cs
@@ -1,22 +1,47 @@
+using System.Text.Json;
 using RedisAPI.Models;
+using StackExchange.Redis;
 
 namespace RedisAPI.Data
 {
     public class RedisPlatformRepo : IPlatformRepo
     {
+        private readonly IConnectionMultiplexer _redis;
+        public RedisPlatformRepo(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+        }
+
         public void CreatePlatform(Platform platform)
         {
-            throw new NotImplementedException();
+            if (platform is null)
+                throw new ArgumentOutOfRangeException(nameof(platform));
+
+            var db = _redis.GetDatabase();
+
+            var serialPlatform = JsonSerializer.Serialize(platform);
+
+            db.StringSet(platform.Id, serialPlatform);
         }
+
 
         public IEnumerable<Platform> GetAllPlatforms()
         {
             throw new NotImplementedException();
         }
 
-        public Platform GetPlatformById(string id)
+        public Platform? GetPlatformById(string id)
         {
-            throw new NotImplementedException();
+            var db = _redis.GetDatabase();
+    
+            var platform = db.StringGet(id);
+
+            if (!string.IsNullOrEmpty(platform))
+            {
+                return JsonSerializer.Deserialize<Platform>(platform);
+            }
+
+            return null;
         }
     }
 }
