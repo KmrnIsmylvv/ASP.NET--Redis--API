ebfcc3cb9571ac4b225474d24003e69d09050917
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-12-30 20:22:50 +0400

    feat: Getting all Platforms

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -22,5 +22,21 @@ namespace RedisAPI.Controllers
 
             return NotFound();
         }
+
+        [HttpPost]
+        public ActionResult<Platform> CreatePlatform(Platform platform)
+        {
+            _repo.CreatePlatform(platform);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platform.Id }, platform);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Platform>> GetPlatforms()
+        {
+            return Ok(_repo.GetAllPlatforms());
+        }
+
     }
+
 }
